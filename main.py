8cea43bff81f2e4b051de8fa2d1417ceed8c8266
unknown <denniswang.pro@gmail.com>
2026-01-12 22:50:20 -0800

    init

---
+++ main.py
@@ -0,0 +1,18 @@
+print("hello world")
+
+print("    /|")
+print("   / |")
+print("  /  |")
+print(" /   |")
+print("/____|")
+
+character_name = "John" # string
+character_age = 35      # number
+is_male = True          # boolean
+print (f"There was once a man named {character_name}")
+print (f"he is {character_age} years old")
+
+print("She exclaimed \"Wow!\"")
+phrase = "Goodbye World"
+print(phrase.upper().isupper())
+print(len(phrase))
